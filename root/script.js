f6139ac53feda1f87d7cc0cdd171fde8a2c663bd
gwyndol1ne <Dragon>
2024-11-20 18:20:55 +0300

    css changes

--- root/script.js
+++ root/script.js
@@ -1,41 +1,23 @@
 const app = Vue.createApp({
-    data() {
+  data() {
       return {
-        popupVisible: false,
-        loading: true,
-        contact: {
-          firstname: "",
-          lastname: "",
-          email: "",
-          message: "",
-        },
-        registerData: {
-          firstname: "",
-          lastname: "",
-          email: "",
-          privacy: false,
-        },
+          showPopup: false,
+          form: {
+              firstname: '',
+              lastname: '',
+              email: ''
+          }
       };
-    },
-    mounted() {
-      setTimeout(() => {
-        this.loading = false;
-      }, 2000);
-    },
-    methods: {
-      sendMessage() {
-        alert("Message sent!");
+  },
+  methods: {
+      togglePopup() {
+          this.showPopup = !this.showPopup;
       },
-      register() {
-        if (!this.registerData.privacy) {
-          alert("Please accept the privacy policy.");
-          return;
-        }
-        alert(`Welcome, ${this.registerData.firstname}!`);
-        this.registerData = { firstname: "", lastname: "", email: "", privacy: false };
-      },
-    },
-  });
-  
-  app.mount("#app");
-  
+      submitForm() {
+          console.log(this.form);
+          this.showPopup = false;
+      }
+  }
+});
+
+app.mount('#app');
